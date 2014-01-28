39ed24d222ed0256a00afaa783a174c73ab523db
Asher Diamant <asher.diamant@gmail.com>
2014-01-28 09:57:59 -0500

    URLs for forms, moved code in view+tests, new URL

--- lists/tests.py
+++ lists/tests.py
@@ -19,11 +19,6 @@ class HomePageTest(TestCase):
     	expected_html = render_to_string('home.html')
     	self.assertEqual(response.content.decode(), expected_html)
     	    	
-    def test_home_page_only_saves_items_when_necessary(self):
-    	request = HttpRequest()
-    	home_page(request)
-    	self.assertEqual(Item.objects.all().count(), 0)
-    	
 
 class itemModelTest(TestCase):
 	
@@ -73,11 +68,10 @@ class NewListTest(TestCase):
 
     
     def test_redirects_after_POST(self):
-		response = self.client.post(
+    	response = self.client.post(
 			'/lists/new',
 			data={'item_text': 'A new list item'}
     	)
-    	self.assertEqual(response.status_code, 302)
-    	self.assertEqual(response['location'],'/lists/the_only_list_in_the_world')     	
+    	self.assertRedirects(response, '/lists/the_only_list_in_the_world')     	
 	
 	
