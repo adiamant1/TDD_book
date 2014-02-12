a6cba6cb9bf864459fafd5a8f1059e290500e127
adiamant1 <asher.diamant@gmail.com>
2014-02-12 15:01:21 -0500

    Add South

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import Item, List
 
 # Create your views here.
@@ -11,17 +11,12 @@ def home_page(request):
 	
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
-	form = ItemForm()
+	form = ExistingListItemForm(for_list=list_)
 	if request.method == 'POST':
-		form = ItemForm(data=request.POST)
+		form = ExistingListItemForm(for_list=list_, data=request.POST)
 		if form.is_valid():
-			try:
-				form.save(for_list=list_)	
-				return redirect(list_)
-			except ValidationError:
-				form.errors.update(
-					{'text': "You've already got this in your list"}	
-				)
+			form.save()	
+			return redirect(list_)
 	return render(request, 'list.html', {'list': list_, "form": form})
 
 def new_list(request):
